fed46287b49cb30b80cef7eb02fc2fbb403c33a5
DinoChew <164445466+DinoChew@users.noreply.github.com>
2024-04-16 13:56:16 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.chq2mwi2a6bq.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "chewlaisoon-employee"
+customregion = "us-east-1"
+
